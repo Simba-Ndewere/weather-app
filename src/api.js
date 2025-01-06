7b327ad7dd19c5acf0e3a55ebe32eaba19ada034
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-06 17:26:27 +0000

    create api class  to get weather and get weather gif

---
+++ src/api.js
@@ -0,0 +1,14 @@
+class API {
+    //write api for getting weather data
+    //write api for weather gif
+
+    static async getWeather() {
+        
+    }
+
+    static getWeatherGif() {
+
+    }
+}
+
+export default API;
