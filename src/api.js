06f761cc4c84076f8d88eb9939bf4317972c868d
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-12 21:53:41 +0000

    create html div container

--- src/api.js
+++ src/api.js
@@ -5,15 +5,11 @@ class API {
     static async getWeather(location) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=9DEP6JU7BXGUFKDLN9BDX9878`, {mode: 'cors'});
         const weatherObject = await response.json();
-        //console.log(weatherObject.description);
-        const weatherDays = weatherObject.days;
-        weatherDays.forEach(element => {
-            console.log(element.datetime);
-        });
+        console.log(weatherObject);
     }
 0
-    static getWeatherGif() {
-
+    static getLocationImage() {
+        
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import api from './api';
 
 api.getWeather('kuala lumpur');
