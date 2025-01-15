2ca77757307cd72bbc8af51854566d5716412a63
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-15 00:12:37 +0000

    create weather class with getters and setters

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,32 @@
 class weather {
-    
+    constructor(temperature, condition, address) {
+        this._temperature = temperature;
+        this._condition = condition;
+        this._address = address;
+    }
+
+    get temperature() {
+        return this._temperature;
+    }
+
+    set temperature(temperature) {
+        this._temperature = temperature;
+    }
+
+    get condition() {
+        return this._condition;
+    }
+
+    set condition(condition) {
+        this._condition = condition;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    set address(address) {
+        this._address = address;
+    }
+
 }
