b1c0ad2050c96cb46259f43c441d2fd1826be9b8
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-17 00:58:09 +0000

    desing cards for weekly weather information

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ class API {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=9DEP6JU7BXGUFKDLN9BDX9878`, {mode: 'cors'});
         const weatherJson = await response.json();
         const weatherObject = new Weather(weatherJson.currentConditions.temp, 
-            weatherJson.currentConditions.icon, weatherJson.resolvedAddress, weatherJson.currentConditions.icon);
+            weatherJson.currentConditions.conditions, weatherJson.resolvedAddress, weatherJson.currentConditions.icon);
         return weatherObject;
     }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import api from './api';
 import dom from './dom'
 
-const weatherObject = await api.getWeather('enfield');
+const weatherObject = await api.getWeather('harare');
 console.log(weatherObject);
 dom.updateLeftContainer(weatherObject);
  
