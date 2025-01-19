82ed34611898bac92fd247058c7638eef2367d54
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-19 14:53:45 +0000

    remove mobile responsive

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ class API {
         let minmax = [];
         let dayIcons = [];
 
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=9DEP6JU7BXGUFKDLN9BDX9878`, { mode: 'cors' });
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=9DEP6JU7BXGUFKDLN9BDX9878&unitGroup=metric`, { mode: 'cors' });
         const weatherJson = await response.json();
 
         weatherJson.days.forEach(day => {
