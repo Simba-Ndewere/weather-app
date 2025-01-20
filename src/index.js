084674dacf6da9ce93d622041d83017aeaad290d
Simba Ndewere <ndeweresimba@gmail.com>
2025-01-20 16:50:45 +0000

    show modal for error handling when invalid location has been entered

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import api from './api';
 import dom from './dom'
 
 const search = document.querySelector(".search");
+const modal = document.querySelector(".modal");
 let location = '';
 
 search.addEventListener('keypress', function (e) {
@@ -14,13 +15,25 @@ search.addEventListener('keypress', function (e) {
 });
 
 const displayWeather = async (location) => {
-    const weather = await api.getWeather(location);
-    dom.updateLeftContainer(weather);
-    dom.updateTopRightContainerDay(weather.days);
-    dom.updateTopRightContainerMinMaxTemp(weather.minmax);
-    dom.updateTopRightContainerDayIcon(weather.dayIcons);
-    dom.updateTodayHighLights(weather);
-    return weather;
+    try {
+        const weather = await api.getWeather(location);
+        dom.updateLeftContainer(weather);
+        dom.updateTopRightContainerDay(weather.days);
+        dom.updateTopRightContainerMinMaxTemp(weather.minmax);
+        dom.updateTopRightContainerDayIcon(weather.dayIcons);
+        dom.updateTodayHighLights(weather);
+        return weather;
+    } catch (error) {
+        showModal();
+    }
+}
+
+function showModal() {
+    modal.classList.add('show');
+
+    setTimeout(() => {
+        modal.classList.remove('show');
+    }, 3000);
 }
 
 window.onload = displayWeather('maidenhead uk');
